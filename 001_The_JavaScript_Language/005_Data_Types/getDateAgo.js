2f0b8fe3eae083f37174d6c821dbf899742456bd
Carlos Martinez <curlosmart@gmail.com>
2022-12-30 15:18:29 -0500

    getDateAgo

---
+++ 001_The_JavaScript_Language/005_Data_Types/getDateAgo.js
@@ -0,0 +1,12 @@
+const getDateAgo = (date, days) => {
+    const dateDaysAgo = new Date(date);
+    dateDaysAgo.setDate(dateDaysAgo.getDate() - days);
+
+    return dateDaysAgo.getDate();
+};
+
+let date = new Date(2015, 0, 2);
+
+alert(getDateAgo(date, 1)); // 1, (1 Jan 2015)
+alert(getDateAgo(date, 2)); // 31, (31 Dec 2014)
+alert(getDateAgo(date, 365)); // 2, (2 Jan 2014)
